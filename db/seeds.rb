cf7ec277afdf59772619ad300472995242aeeb90
langdag <lucky10cents@gmail.com>
2017-08-15 12:51:04 +0300

    Modified seed file and seeded the db with its contents

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+store_list =[ 
+	["Spain", "Zara", "Paseo de la Castellana 79", "28020 Madrid"], 
+	["France", "Le Géant des Beaux Arts Paris 11", "166 Rue de la Roquette", "75011 Paris"],
+	["Germany", "REWE", "Westring 42", "34127 Kassel"]
+]
+
+store_list.each do |country, name, address, city|
+	Store.create(country: country, name: name, address: address, city: city)
+end
